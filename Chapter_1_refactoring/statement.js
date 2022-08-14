a44cfa3e92a1d96109464a1e129145907ae86132
Akhilesh Prathuri <akhilnaruto@users.noreply.github.com>
2022-08-14 05:01:20 +0000

    more refactorings

--- Chapter_1_refactoring/statement.js
+++ Chapter_1_refactoring/statement.js
@@ -2,60 +2,72 @@ var fs = require('fs');
 
 
 
-function amountFor(perf, play){
-    let thisAmount = 0;
-    switch(play.type){
-        case "tragedy":
-            thisAmount = 40000;
-            if(perf.audience > 30){
-                thisAmount += 1000 * (perf.audience - 30);
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000;
-            if(perf.audience > 20){
-                thisAmount += 10000 + 500 * (perf.audience - 20);
-            }   
-            thisAmount += 300 * perf.audience; 
-            break;
-
-        default:
-            throw new Error(`Unknown type : ${play.type}`);    
-    }
-    return thisAmount;
+function usd(aNumber){
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2
+    }).format(aNumber/100);
 }
 
-function statement(invoice, plays) {
-    let totalAmount = 0;
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+}
+
+function totalVolumeCredits(){
     let volumeCredits = 0;
-
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format;
-
-    for(let perf of invoice.performances){
-        const play =  plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
-
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // add extra credit for every ten comedy attendeeas
-        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience/5);
-
-        //print the line for this order
-         result += `  ${play.name} : ${format(thisAmount/100)} (${perf.audience} seats) \n`;
-         totalAmount += thisAmount;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
+   return volumeCredits;
+}
 
-    result += `Amount owed is ${format(totalAmount/100)} \n`;
-    result += ` You earned ${volumeCredits} credits \n`;
+function volumeCreditsFor(aPerformance) {
+    let result = Math.max(aPerformance.audience - 30, 0);;
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+
     return result;
 }
 
-var invoice = JSON.parse(fs.readFileSync('Chapter_1_refactoring/invoices.json', 'utf8'));
+function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+        case "tragedy":
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+            result += 300 * aPerformance.audience;
+            break;
+
+        default:
+            throw new Error(`Unknown type : ${playFor(aPerformance).type}`);
+    }
+    return result;
+}
+
+function statement(invoice) {
+    let totalAmount = 0;
+
+    let result = `Statement for ${invoice.customer}\n`;
+
+    for (let perf of invoice.performances) {
+        //print the line for this order
+        result += `  ${playFor(perf).name} : ${usd(amountFor(perf) / 100)} (${perf.audience} seats) \n`;
+        totalAmount += amountFor(perf);
+    }
+
+    result += `Amount owed is ${usd(totalAmount / 100)} \n`;
+    result += ` You earned ${totalVolumeCredits()} credits \n`;
+    return result;
+}
+
+var invoice = JSON.parse(fs.readFileSync('Chapter_1_refactoring/invoices.json', 'utf8'))[0];
 var plays = JSON.parse(fs.readFileSync('Chapter_1_refactoring/plays.json', 'utf8'));
 
-console.log(statement(invoice[0], plays));
+console.log(statement(invoice, plays));
 
